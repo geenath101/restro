550cf06b9f4d1aedba4e6bbb735ed44f60c07ee3
geenath <dlgpforwork24@gmail.com>
2025-09-09 11:57:14 +1200

    fixed the issue with qr and changed the background to white

--- app/page.tsx
+++ app/page.tsx
@@ -17,15 +17,18 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold mb-8">Scan to view the menu</h1>
       {isClient && url ? (
-        <QRCodeCanvas value={url} size={256} />
+        <>
+          <h1>{url}</h1>
+          <QRCodeCanvas value={url} size={256} />
+        </>
       ) : (
         <div style={{ width: 256, height: 256 }} />
       )}
-      <p className="mt-8">
+      {/* <p className="mt-8">
         <Link href="/menu" className="text-blue-500 hover:underline">
           Or click here to view the menu
         </Link>
-      </p>
+      </p> */}
     </main>
   );
 }
